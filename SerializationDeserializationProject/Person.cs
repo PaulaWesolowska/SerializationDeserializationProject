db61740aa38340984114bea3ad015c4fd4b18af1
paula <paula.anna.wesolowska@gmail.com>
2022-06-25 11:56:43 +0200

    Dodaj pliki projektów.

---
+++ SerializationDeserializationProject/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerializationDeserializationProject
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Index { get; set; }
+
+
+        public void SaveFile(string fileName)
+        {
+            using (var stream = new FileStream(fileName, FileMode.Create))
+            {
+                var serializer = new XmlSerializer(typeof(Person));
+                serializer.Serialize(stream, this);
+            }
+        }
+        public static Person LoadFile(string fileName)
+        {
+            using (var stream = new FileStream(fileName, FileMode.Open))
+            {
+                var deserializer = new XmlSerializer(typeof(Person));
+                return (Person)deserializer.Deserialize(stream);
+            }
+        }
+    }
+}
---
+++ SerializationDeserializationProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerializationDeserializationProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Paula";
+            person.LastName = "Wesolowska";
+            person.Index = 56694;
+            person.SaveFile("PersonalData.xml");
+            Person load = Person.LoadFile("PersonalData.xml");
+        }
+    }
+}
